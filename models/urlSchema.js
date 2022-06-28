7eaf76d419770721f3b5e66e4722fa87d6bca5b7
AShaww <71328022+AShaww@users.noreply.github.com>
2022-06-28 22:16:57 +0100

    updated urlSchema.js - fixed bug on same generated ID for multiple entries

--- models/urlSchema.js
+++ models/urlSchema.js
@@ -1,8 +1,9 @@
-const { mongoose, Schema } = require('mongoose')
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 const crypto = require('crypto')
 let nanoid = (t = 21) => crypto.getRandomValues(new Uint8Array(t)).reduce(((t, e) => t += (e &= 63) < 36 ? e.toString(36) : e < 62 ? (e - 26).toString(36).toUpperCase() : e > 62 ? "-" : "_"), "");
 
-const generateID = nanoid();
+
 
 const generatedURLSchema = new Schema({
     long: {
@@ -11,8 +12,9 @@ const generatedURLSchema = new Schema({
     },
     short: {
         type: String,
+        unique: true,
+        default: () => nanoid(8),
         required: true,
-        default: generateID
     },
     clicks: {
         type: Number,

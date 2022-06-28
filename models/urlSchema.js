4e03faf946bfc272121cced7ecce4eabe9081edb
AShaww <71328022+AShaww@users.noreply.github.com>
2022-06-28 22:00:52 +0100

    added nano id to schema

--- models/urlSchema.js
+++ models/urlSchema.js
@@ -1,7 +1,10 @@
-const { mongoose, Schema, mongo } = require('mongoose')
+const { mongoose, Schema } = require('mongoose')
+const crypto = require('crypto')
+let nanoid = (t = 21) => crypto.getRandomValues(new Uint8Array(t)).reduce(((t, e) => t += (e &= 63) < 36 ? e.toString(36) : e < 62 ? (e - 26).toString(36).toUpperCase() : e > 62 ? "-" : "_"), "");
 
+const generateID = nanoid();
 
-const generatedURLSchema = mongoose.Schema({
+const generatedURLSchema = new Schema({
     long: {
         type: String,
         required: true
@@ -9,7 +12,7 @@ const generatedURLSchema = mongoose.Schema({
     short: {
         type: String,
         required: true,
-        default: "jli2k213"
+        default: generateID
     },
     clicks: {
         type: Number,
